16072494e4e802c75a295901fc50833aefdac537
Marian Krekoten <krekoten@gmail.com>
2023-07-21 23:30:14 +0300

    Center QuestionScreen

--- src/QuestionScreen/index.tsx
+++ src/QuestionScreen/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Button from "@mui/material/Button"
+import {Box, Button } from "@mui/material"
 
 import QuestionText from "./QuestionText"
 import Answers from "./Answers"
@@ -13,11 +13,11 @@ interface QuestionParams {
 }
 
 function QuestionScreen({text, answers}: QuestionParams): JSX.Element {
-  return <>
+  return <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
     <QuestionText text={text} />
     <Answers answers={answers} />
     <Button className="next" variant="contained" color="primary">Next</Button>
-  </>
+  </Box>
 }
 
 export default QuestionScreen
